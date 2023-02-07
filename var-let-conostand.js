fa118d3eb6cd5515dd25952cde7651f8376bfa3b
alzabir <alzabirhussain@gmail.com>
2023-02-07 08:39:50 -0800

    var-let-conostand.js

---
+++ var-let-conostand.js
@@ -0,0 +1,26 @@
+// var / let / const  er basic alocona 
+var age = 10 ;
+age = 15 ;
+console.log(age) ;
+//  out put asbe  (15)
+
+// conostand a kono kicur man change hobe naah (pai er man = 3.1416 eta jobono change hobe nah )
+// thik tamon conostand a ja prothom a dibo setai hobe  
+const country_name = "Bangladesh";
+console.log(country_name);
+
+//////////////////////////////////////////
+
+
+// var a jodi  2 ta name  ekui dei tahole porer ta output asbe 
+var age = 20 ;
+var age = 30 ;
+console.log(age);
+///////////////////////////////////////////////////////////////
+
+
+// let a jodi 2 tar name same hoi tahole out put asbe nah 
+let age = 10 ;
+let age = 20 ;
+console.log(age);
+////////////////////////////////////////////////////////////
